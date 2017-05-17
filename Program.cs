4903622dd10d2a8b6b356251594a03615ccf7edd
Jöran Malek <joeran3@gmail.com>
2017-05-17 13:04:37 +0200

    Implement Renderer

--- Program.cs
+++ Program.cs
@@ -12,17 +12,19 @@ namespace Analysis
             var app = new CommandLineApplication();
 
             var logTypeArgument = app.Argument("logType", "Which Log Type? Possible values: <windows|macos>", false);
+            var rendererTypeArgument = app.Argument("rendererType", "Which renderer should be used? Possible values: <visjs>", false);
             var logFileOption = app.Option("-l|--log", "Log File to be analyzed", CommandOptionType.SingleValue);
             var outFileOption = app.Option("-o|--out", "Output File", CommandOptionType.SingleValue);
-            var jsonFormatOption = app.Option("--pretty", "Pretty print json", CommandOptionType.NoValue);
+            var rendererTemplate = app.Option("-t|--template", "Template for <visj> renderer.", CommandOptionType.SingleValue);
+
             app.HelpOption("-?|--help");
 
             app.OnExecute(() =>
             {
-                var logTypeValue = logTypeArgument.Value;
                 var logAnalyzer = default(ILogAnalyzer);
+                var logRenderer = default(ILogRenderer);
 
-                switch (logTypeValue)
+                switch (logTypeArgument.Value)
                 {
                     case "windows":
                         logAnalyzer = new WindowsLogAnalyzer(app.Out);
@@ -30,6 +32,15 @@ namespace Analysis
 
                     case "macos":
                         logAnalyzer = new MacOSLogAnalyzer(app.Out);
+                        break;
+
+                    default:
+                        throw new ArgumentException();
+                }
+                switch (rendererTypeArgument.Value)
+                {
+                    case "visjs":
+                        logRenderer = new VisjsLogRenderer();
                         break;
 
                     default:
@@ -43,8 +54,15 @@ namespace Analysis
                     while (!reader.EndOfStream)
                     {
                         var line = reader.ReadLine();
-                        app.Out.WriteLine(line);
-                        logAnalyzer.ResolveLine(line);
+                        try
+                        {
+                            logAnalyzer.ResolveLine(line);
+                        }
+                        catch (Exception e)
+                        {
+                            app.Out.WriteLine(line);
+                            throw e;
+                        }
                     }
                 }
 
@@ -53,16 +71,15 @@ namespace Analysis
                     app.Out.WriteLine($"Did not close {item.Group.Name} {item.Timestamp} {item.Title}");
                 }
 
-                var formatting = jsonFormatOption.HasValue() ? Formatting.Indented : Formatting.None;
-                var groupsData = $"var groupData = {JsonConvert.SerializeObject(logAnalyzer.Groups.Values, formatting)};";
-                var itemsData = $"var itemsData = {JsonConvert.SerializeObject(logAnalyzer.Items, formatting)};";
-
                 var file = "data.js";
                 if (outFileOption.HasValue())
                 {
                     file = outFileOption.Value();
                 }
-                File.WriteAllText(file, $"{groupsData}\n{itemsData}");
+
+                using (var stream = File.OpenWrite(file))
+                using (var writer = new StreamWriter(stream))
+                    logRenderer.Render(writer, logAnalyzer.Groups.Values, logAnalyzer.Items, rendererTemplate);
 
                 return 0;
             });

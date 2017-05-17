afd4287e0e975ab20b373fc6693603e1048b6605
Jöran Malek <joeran3@gmail.com>
2017-05-17 13:03:49 +0200

    Add ILogRenderer

---
+++ ILogRenderer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Analysis
+{
+    interface ILogRenderer
+    {
+        void Render(TextWriter writer, IEnumerable<Group> groups, IEnumerable<Item> items, CommandOption template);
+    }
+}
